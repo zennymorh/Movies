31ee927502c706a95b146efa460f5a5b5d84b9e0
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-05-23 02:48:33 +0100

    Implemented API Service

---
+++ app/src/main/java/com/zennymorh/movies/APIService.kt
@@ -0,0 +1,40 @@
+package com.zennymorh.movies
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+private const val BASE_URL = "https://api.themoviedb.org/3/"
+private const val API_KEY = "d5c2a0fd5dc0cb6f0b41de11ec78d19b"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MovieAPIService {
+    @GET("movie/top_rated?api_key=$API_KEY")
+    suspend fun getTopRatedMovies():
+            ArrayList<MovieResult>
+
+    @GET("movie/popular?api_key=$API_KEY")
+    suspend fun getPopularMovies():
+            ArrayList<MovieResult>
+
+    @GET("movie/{id}/videos?api_key=$API_KEY")
+    suspend fun getMovieTrailers():
+            ArrayList<TrailerResult>
+
+    @GET("movie/{id}/reviews?api_key=$API_KEY")
+    suspend fun getReviews(@Path("id") id: String):
+            ArrayList<ReviewResult>
+}
+
+object APIService {
+    val retrofitService : MovieAPIService by lazy {
+        retrofit.create(MovieAPIService::class.java)
+    }
+}
---
+++ app/src/main/java/com/zennymorh/movies/Data.kt
@@ -0,0 +1,63 @@
+package com.zennymorh.movies
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    val id: Int,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    val adult: Boolean,
+    val overview: String,
+    val releaseDate: String,
+    val title: String
+): Parcelable
+
+@Parcelize
+data class MovieResult(
+    val page: Int,
+    val movie: ArrayList<Movie>,
+    @SerializedName("total_results")
+    val totalResult: Int,
+    @SerializedName("total_pages")
+    val totalPages: Int
+): Parcelable
+
+@Parcelize
+data class TrailerResult(
+    val id: Int,
+    val results: ArrayList<Trailer>
+): Parcelable
+
+@Parcelize
+data class Trailer(
+    val id: String,
+    val iso_639_1: String,
+    val iso_3166_1: String,
+    val key: String,
+    val name: String,
+    val site: String,
+    val size: Int,
+    val type: String
+): Parcelable
+
+@Parcelize
+data class ReviewResult(
+    val id: Int,
+    val page: Int,
+    val results: ArrayList<Review>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults:Int
+): Parcelable
+
+@Parcelize
+data class Review(
+    val id: String,
+    val author: String,
+    val content: String,
+    val url: String
+): Parcelable
