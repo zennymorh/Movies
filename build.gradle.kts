ed9887bfd29f19e37cde9caeeb036911f2c98bbc
zennymorh <jimohzainab4753@gmail.coom>
2025-06-29 16:07:49 +0200

    Add detekt and ktlint dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,14 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
+}
 
+detekt {
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    source.setFrom(files(project.projectDir))
+    // Optional: Set the baseline file for ignoring existing issues (run ./gradlew detektBaseline first)
+    // baseline = file("$rootDir/config/detekt/detekt-baseline.xml")
 }
